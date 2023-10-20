eceb1b133a691e7b42b1ef011f3d11fd5561145f
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-10-20 14:49:31 +0200

    TP3 exo1 done

--- src/main/java/fr/diginamic/jdbc/TestInsertion.java
+++ src/main/java/fr/diginamic/jdbc/TestInsertion.java
@@ -1,16 +1,15 @@
-package connexion;
+package fr.diginamic.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class TestConnexionJdbc {
+public class TestInsertion {
 
 	public static void main(String[] args) throws SQLException {
-		// TODO Auto-generated method stub
 		DriverManager.registerDriver(new org.mariadb.jdbc.Driver());
-		String url = "jdbc:mariadb://localhost:3306/bdd_test";
+		String url = "jdbc:mariadb://localhost:3306/campta";
 		String utilisateur = "root";
 		String motDePasse = "";
 
@@ -20,13 +19,17 @@ public class TestConnexionJdbc {
 	            connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
 
 	            System.out.println("Connexion à la base de données compta établie.");
-	            System.out.println("Connexion : " + connexion);
 	            
-	            Statement statement = connexion.createStatement();
-	            String req = "INSERT INTO user (name,email) values ('Harry', 'potter@gmail.com')";
-	            statement.executeUpdate(req);
-	            statement.close();
+	         
 
+	            
+	            
+	            
+	            
+	            
+	            
+	            
+	            
 	        } catch (SQLException e) {
 	            System.err.println("Erreur lors de la connexion à la base de données : " + e.getMessage());
 	        } finally {
