dd9201bdceb196f014f932ec339893d6367c5e13
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-10-20 15:20:31 +0200

    TP4 Dao setup

---
+++ src/main/java/fr/diginamic/jdbc/dao/FournisseurDaoJdbc.java
@@ -0,0 +1,33 @@
+package fr.diginamic.jdbc.dao;
+
+import java.util.List;
+
+import fr.diginamic.jdbc.entites.Fournisseur;
+
+public class FournisseurDaoJdbc implements FournisseurDao{
+
+	@Override
+	public List<Fournisseur> extraire() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(Fournisseur fournisseur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int update(String ancienNom, String nouveauNom) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean delete(Fournisseur fournisseur) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
