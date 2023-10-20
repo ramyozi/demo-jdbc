58f25c7b11581d9279e9ec19fb1c75dfdad2a510
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-10-20 15:02:31 +0200

    TP3 exo2 TestUpdate

--- src/main/java/fr/diginamic/jdbc/TestUpdate.java
+++ src/main/java/fr/diginamic/jdbc/TestUpdate.java
@@ -1,5 +1,54 @@
 package fr.diginamic.jdbc;
 
-public class TestUpdate {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class TestUpdate {
+	public static void main(String[] args) throws SQLException {
+		DriverManager.registerDriver(new org.mariadb.jdbc.Driver());
+		String url = "jdbc:mariadb://localhost:3306/campta";
+		String utilisateur = "root";
+		String motDePasse = "";
+
+		Connection connexion = null;
+
+		try {
+			connexion = DriverManager.getConnection(url, utilisateur,
+					motDePasse);
+
+			System.out.println(
+					"Connexion à la base de données compta établie.");
+			
+			Statement statement = connexion.createStatement();
+            String req = "UPDATE fournisseur SET NOM = 'La Maison des Peintures' WHERE ID = 0";
+            int rowCount = statement.executeUpdate(req);
+
+            if (rowCount > 0) {
+                System.out.println("Mise à jour effectuée avec succès.");
+            } else {
+                System.out.println("Aucune ligne mise à jour.");
+            }
+            
+			statement.close();
+
+		} catch (SQLException e) {
+			System.err.println(
+					"Erreur lors de la connexion à la base de données : "
+							+ e.getMessage());
+		} finally {
+			if (connexion != null) {
+				try {
+					connexion.close();
+					System.out.println(
+							"Connexion à la base de données bdd_test fermée.");
+				} catch (SQLException e) {
+					System.err.println(
+							"Erreur lors de la fermeture de la connexion : "
+									+ e.getMessage());
+				}
+			}
+		}
+	}
 }
