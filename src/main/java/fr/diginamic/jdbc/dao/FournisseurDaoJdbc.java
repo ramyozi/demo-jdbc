84c9ec16814706b6270df18e33d44639d5b9aa4d
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-10-20 15:26:37 +0200

    TP4 FournisseurDaoJdbc methods

--- src/main/java/fr/diginamic/jdbc/dao/FournisseurDaoJdbc.java
+++ src/main/java/fr/diginamic/jdbc/dao/FournisseurDaoJdbc.java
@@ -1,33 +1,79 @@
 package fr.diginamic.jdbc.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import fr.diginamic.jdbc.entites.Fournisseur;
 
-public class FournisseurDaoJdbc implements FournisseurDao{
+public class FournisseurDaoJdbc implements FournisseurDao {
+    private String url = "jdbc:mysql://localhost:3306/compta";
+    private String utilisateur = "votreUtilisateur";
+    private String motDePasse = "votreMotDePasse";
 
-	@Override
-	public List<Fournisseur> extraire() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<Fournisseur> extraire() {
+        List<Fournisseur> fournisseurs = new ArrayList<>();
 
-	@Override
-	public void insert(Fournisseur fournisseur) {
-		// TODO Auto-generated method stub
-		
-	}
+        try (Connection connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
+             Statement statement = connexion.createStatement()) {
+            String req = "SELECT ID, NOM FROM fournisseur";
+            ResultSet resultSet = statement.executeQuery(req);
 
-	@Override
-	public int update(String ancienNom, String nouveauNom) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+            while (resultSet.next()) {
+                int id = resultSet.getInt("ID");
+                String nom = resultSet.getString("NOM");
+                Fournisseur fournisseur = new Fournisseur(id, nom);
+                fournisseurs.add(fournisseur);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-	@Override
-	public boolean delete(Fournisseur fournisseur) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+        return fournisseurs;
+    }
 
+    @Override
+    public void insert(Fournisseur fournisseur) {
+        try (Connection connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
+             PreparedStatement statement = connexion.prepareStatement("INSERT INTO fournisseur (NOM) VALUES (?)")) {
+            statement.setString(1, fournisseur.getNom());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int update(String ancienNom, String nouveauNom) {
+        int rowCount = 0;
+        try (Connection connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
+             PreparedStatement statement = connexion.prepareStatement("UPDATE fournisseur SET NOM = ? WHERE NOM = ?")) {
+            statement.setString(1, nouveauNom);
+            statement.setString(2, ancienNom);
+            rowCount = statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rowCount;
+    }
+
+    @Override
+    public boolean delete(Fournisseur fournisseur) {
+        boolean deleted = false;
+        try (Connection connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
+             PreparedStatement statement = connexion.prepareStatement("DELETE FROM fournisseur WHERE ID = ?")) {
+            statement.setInt(1, fournisseur.getId());
+            int rowCount = statement.executeUpdate();
+            deleted = rowCount > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return deleted;
+    }
 }
