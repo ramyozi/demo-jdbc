bf0a8693bd7993e63e46bbefb393b28a62467750
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-10-20 11:18:57 +0200

    Connection to the DB has been established

--- src/main/java/connexion/TestConnexionJdbc.java
+++ src/main/java/connexion/TestConnexionJdbc.java
@@ -1,10 +1,31 @@
 package connexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TestConnexionJdbc {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
+		DriverManager.registerDriver(new org.mariadb.jdbc.Driver());
+		String url = "jdbc:mariadb://localhost:3306/bdd_test";
+		String utilisateur = "root";
+		String motDePasse = "";
 
+		Connection connexion = null;
+
+		try {
+			connexion = DriverManager.getConnection(url, utilisateur,
+					motDePasse);
+
+			System.out.println(
+					"Connexion à la base de données bdd_test établie.");
+
+		} catch (SQLException e) {
+			System.err.println(
+					"Erreur lors de la connexion à la base de données : "
+							+ e.getMessage());
+		}
 	}
-
 }
